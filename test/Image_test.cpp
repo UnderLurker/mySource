75a02f1fd4c809136bbd3469af863048720dd5f8
UnderLurker <63385072+UnderLurker@users.noreply.github.com>
2023-04-18 16:33:36 +0800

    Update Image_test.cpp

--- test/Image_test.cpp
+++ test/Image_test.cpp
@@ -21,7 +21,7 @@ void print(double** input,int n){
 }
 
 int main(){
-	string str="../img/Image/1.jpg";
+	string str="../img/Image/3.jpg";
 	JPEGData data;
 	clock_t startTime=clock();
 	data.readJPEG(str.c_str());
@@ -29,13 +29,13 @@ int main(){
 	BMPData bmp(data.getRGBMatrix(),data.getWidth(),data.getHeight(),true);
 	bmp.GrayEncoder();
 	bmp.GaussianHandle(false,[](double in){
-		return in<THRESHOLD?255.0:0.0;
+		return in>THRESHOLD?255.0:0.0;
 		// return in;
 	});
 	bmp.saveBMP("gray.bmp");
 	double m1[][3]={{-1,0,1},{-1,0,1},{-1,0,1}},
 		   m2[][3]={{-1,-1,-1},{0,0,0},{1,1,1}};
-	bmp.EdgeDetect(false,m1,m2,3);
+	bmp.EdgeDetectPrewitt();
 	bmp.saveBMP("edge.bmp");
 	cout<<dec<<clock()-startTime<<"ms"<<endl;
     return 0;
