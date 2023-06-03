2a5cb47709a4ce14033e43fb69c4839735a344f4
UnderLurker <lnucxn@gmail.com>
2023-06-03 20:18:47 +0800

    增加了大量静态数组，用来方便编码，其中纠错码部分最长

--- utils/Image/src/QrEncode.cpp
+++ utils/Image/src/QrEncode.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <bitset>
 #include <stack>
+#include <string>
 
 NAME_SPACE_START(myUtil)
 
@@ -20,11 +21,20 @@ Matrix<RGB> QREncode::encoding(const string &encodeData){
     else if(type==DataType::AlpNumMode) code.append(AlpNumModeEncoding());
     else if(type==DataType::ByteMode) code.append(ByteModeEncoding());
     else if(type==DataType::KanjiMode) code.append(KanjiModeEncoding());
-
+    fillEncodeData(code);
 
 
     return imgData==nullptr?Matrix<RGB>():*imgData;
 }
+
+Matrix<RGB> QREncode::encoding(const wstring& encodeWData){
+    this->encodeWData=encodeWData;
+    if(!init()) return Matrix<RGB>();
+    imgData=new Matrix<RGB>(getSideLen(),getSideLen(),RGB_WHITE);
+
+    return imgData==nullptr?Matrix<RGB>():*imgData;
+}
+
 //默认找最小的版本
 bool QREncode::init(){
     int NumMode=0,AlpNumMode=0,ByteMode=0;
@@ -108,8 +118,95 @@ string QREncode::ByteModeEncoding(){
     return res;
 }
 string QREncode::KanjiModeEncoding(){
+    // 1.Only for Double-Byte Shift JIS Characters
+    // 2.Convert to Bytes
+    // 3.Encode the Bytes with Kanji Mode
+    //     For characters whose bytes are in the range 0x8140 to 0x9FFC
+    //      for example: 0x89D7 
+    //     1. 0x89D7 - 0x8140 = 0x0897
+    //     2. (0x08 * 0xC0) + 0x97 = (0x600) + 0x97 = 0x697
+    //     3. 0x697 -> binary(13bit)
+    //     For characters whose bytes are in the range 0xE040 to 0xEBBF
+    //      for example: 0xE4AA 
+    //     1. 0xE4AA - 0xC140 = 0x236A
+    //     2. (0x23 * 0xC0) + 0x6A = (0x1A40) + 0x6A = 0x1AAA
+    //     3. 0x1AAA -> binary(13bit)
+    // 4.Put the 13-bit Binary Numbers Together
+    // 5.Next: Finish the Data Encoding Step
     string res="";
+    for(int i=0;i<encodeWData.size();i++){
+
+    }
     return res;
+}
+
+void QREncode::fillEncodeData(string& code){
+    // 1. Determine the Required Number of Bits for this QR Code
+    // 2. Add a Terminator of 0s if Necessary
+    // 3. Add More 0s to Make the Length a Multiple of 8
+    // 4. Add Pad Bytes if the String is Still too Short
+    //  11101100 00010001//两字节循环填入
+    // code string bit length
+    int maxLen=ErrorCurrentTable[(version-1)*4+(int)level][0]*8;
+    int diff=maxLen-code.size();
+    if(diff<0) return;
+    // Add a Terminator
+    if(diff>4) code.append("0000");
+    else if(diff<=4) code.append(string(diff,'0'));
+    // Add More 0s to Make the Length a Multiple of 8
+    int eightDiff = 8-code.size()%8;
+    code.append(string(eightDiff,'0'));
+    // Add Pad Bytes
+    if(maxLen-code.size()<8) return;
+    for(int i=0;i<(maxLen-code.size())/8;i++){
+        if(i%2==0) code.append("11101100");
+        else code.append("00010001");
+    }
+    errorCurrentEncoding(code);
+}
+
+void QREncode::errorCurrentEncoding(string& code){
+    // Step 1: Break Data Codewords into Blocks if Necessary
+    // Step 2: Understand Polynomial Long Division
+    // Step 3: Understand The Galois Field
+    // Step 4: Understand Galois Field Arithmetic
+    // Step 5: Generate Powers of 2 using Byte-Wise Modulo 100011101
+    // Step 6: Understand Multiplication with Logs and Antilogs
+    // Step 7: Understanding The Generator Polynomial
+    // Step 8: Generating Error Correction Codewords
+    // Step 9: Divide the Message Polynomial by the Generator Polynomial
+    int row=(version-1)*4+(int)level,codePos=0;
+    int groupCount = ErrorCurrentTable[row][4]==0?1:2;
+    int*** group=new int**[groupCount];
+    // first group
+    group[0]=new int*[ErrorCurrentTable[row][2]];
+    for(int blockOfGroup=0;blockOfGroup<ErrorCurrentTable[row][2];blockOfGroup++){
+        group[0][blockOfGroup]=new int[ErrorCurrentTable[row][3]];
+        for(int blocks=0;blocks<ErrorCurrentTable[row][3]&&codePos<=code.size();blocks++){
+            int t=0;
+            for(int k=0;k<8;k++){
+                t<<=1;
+                t+=code[codePos+k]-'0';
+            }
+            group[0][blockOfGroup][blocks]=t;
+            codePos+=8;
+        }
+    }
+    // second group
+    if(groupCount==1) return;
+    group[1]=new int*[ErrorCurrentTable[row][4]];
+    for(int blockOfGroup=0;blockOfGroup<ErrorCurrentTable[row][4];blockOfGroup++){
+        group[1][blockOfGroup]=new int[ErrorCurrentTable[row][3]];
+        for(int blocks=0;blocks<ErrorCurrentTable[row][5]&&codePos<=code.size();blocks++){
+            int t=0;
+            for(int k=0;k<8;k++){
+                t<<=1;
+                t+=code[codePos+k]-'0';
+            }
+            group[1][blockOfGroup][blocks]=t;
+            codePos+=8;
+        }
+    }
 }
 
 NAME_SPACE_END()
