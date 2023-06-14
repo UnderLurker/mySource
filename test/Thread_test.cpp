78cb2290b318a2e97ae81a7d1a3c23ab1cb8ab37
UnderLurker <lnucxn@gmail.com>
2023-06-14 22:04:38 +0800

    重新配置了编译器环境，解决了不能使用thread的问题

--- test/Thread_test.cpp
+++ test/Thread_test.cpp
@@ -1,14 +1,33 @@
 // #include "Thread.h"
 #include <iostream>
+#include <mutex>
 #include <thread>
+#include <chrono>
+#include <vector>
 using namespace std;
 // using namespace myUtil;
+mutex mtx;
 void print(){
-    cout<<"abdfsf"<<endl;
+    unique_lock<mutex> lock(mtx);
+    for(int i=0;i<100;i++){
+        cout<<i<<" ";
+    }
 }
+
+struct A{
+    void operator()(int a){
+        for(int i=0;i<100;i++){
+            cout<<a<<":"<<i<<" ";
+        }
+    }
+};
 int main(){
     // ThreadPool pool;
-    thread a(print);
-    a.join();
+    vector<thread> t;
+    t.resize(3);
+    for(int i=0;i<3;i++){
+        t.at(i)=thread((A()),i);
+    }
+    this_thread::sleep_for(chrono::seconds(3));
     return 0;
 }
