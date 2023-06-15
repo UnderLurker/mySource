61b02511e0dfa4179bcb53f84e733677f637bbe8
UnderLurker <63385072+UnderLurker@users.noreply.github.com>
2023-06-15 10:56:40 +0800

    Update Thread.cpp

--- utils/Thread/src/Thread.cpp
+++ utils/Thread/src/Thread.cpp
@@ -1,6 +1,17 @@
 #include "Thread.h"
 #include "Util.h"
+#include <mutex>
+#include <condition_variable>
 
 NAME_SPACE_START(myUtil)
+
+void ThreadPool::pushThread(ThreadPoolFunc func,void* args,int argsLen){
+    unique_lock<mutex> lock(_threadPoolMutex);
+    if(_threadPool.size()>=_threadMaxSize) return;
+    ThreadPoolFuncAndArgs tpFA(func,args,argsLen);
+    _threadPool.emplace_back(tpFA);
+    _cv.notify_one();
+}
+
 
 NAME_SPACE_END()
