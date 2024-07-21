e8b9b8cc0e07fdd4f87296cb32405ccb8f96e13b
UnderLurker <lnucxn@gmail.com>
2024-07-21 12:41:05 +0800

    update third_part remote

--- ULGui/base/include/font.h
+++ ULGui/base/include/font.h
@@ -5,13 +5,25 @@
 #ifndef _FONT_H
 #define _FONT_H
 
+#include <cstdint>
+
+#include "dimension.h"
+
 namespace ULGui::base {
+
+#define FONT_DEFAULT_FAMILY ""
+
+enum FontWeight : size_t {
+    Normal = 0
+};
 
 class ULFont {
 private:
-
+    FontWeight _weight { Normal };
+    Dimension _size {0};
+    std::string _fontFamily { FONT_DEFAULT_FAMILY };
 };
 
-}
+} // namespace ULGui::base
 
 #endif // _FONT_H
--- ULGui/base/include/geometry.h
+++ ULGui/base/include/geometry.h
@@ -34,10 +34,9 @@ public:
         return *this;
     }
     constexpr bool operator==(const Offset<T>& obj) const { return _x == obj._x && _y == obj._y; }
-
     constexpr bool operator!=(const Offset<T>& obj) const { return _x != obj._x || _y != obj._y; }
 
-    std::string toString() const { return "[" + _x.toString() + "," + _x.toString() + "]"; }
+    std::string toString() const { return "[ " + std::to_string(_x) + ", " + std::to_string(_y) + " ]"; }
 
 private:
     T _x;
@@ -45,12 +44,83 @@ private:
 };
 
 template<typename T>
-class Size {};
+class Size {
+public:
+    Size() = default;
+    constexpr explicit Size(T width, T height)
+        : _width(width), _height(height) {}
+
+    T width() const { return _width; }
+    T height() const { return _height; }
+    void setWidth(T width) { _width = width; }
+    void setHeight(T height) { _height = height; }
+
+    constexpr Size<T> operator+(const Size<T>& obj) const { return Size {_width + obj._width, _height + obj._height}; }
+    Size<T>& operator+=(const Size<T>& obj) {
+        _width  += obj._width;
+        _height += obj._height;
+        return *this;
+    }
+
+    constexpr Size<T> operator-(const Size<T>& obj) const { return Size {_width - obj._width, _height - obj._height}; }
+    Size<T>& operator-=(const Size<T>& obj) {
+        _width  -= obj._width;
+        _height -= obj._height;
+        return *this;
+    }
+    constexpr bool operator==(const Offset<T>& obj) const { return _width == obj._width && _height == obj._height; }
+    constexpr bool operator!=(const Offset<T>& obj) const { return _width != obj._width || _height != obj._height; }
+
+    std::string toString() const { return "[ " + std::to_string(_width) + " x " + std::to_string(_height) + " ]"; }
+
+private:
+    T _width;
+    T _height;
+};
+
 
 template<typename T>
-class Rect {};
+class Rect {
+public:
+    Rect() = default;
+    explicit Rect(const Offset<T>& offset, const Size<T>& size)
+        : _offset(offset), _size(size) {}
+
+    Offset<T> offset() const { return _offset; }
+    Size<T> size() const { return _size; }
+    void setOffset(const Offset<T>& offset) { _offset = offset; }
+    void setSize(const Size<T>& size) { _size = size; }
+
+    std::string toString() const { return _offset.toString() + _size.toString(); }
+
+private:
+    Offset<T> _offset;
+    Size<T> _size;
+};
 
 using OffsetF = Offset<float>;
+using SizeF   = Size<float>;
+using RectF   = Rect<float>;
+using PointF  = Offset<float>;
+
+template<typename T>
+class Circle {
+public:
+    Circle() = default;
+    explicit Circle(const Offset<T>& center, T radius) : _center(center), _radius(radius){}
+
+    Offset<T> center() const { return _center; }
+    T radius() const { return _radius; }
+    void setCenter(const Offset<T>& center) { _center = center; }
+    void setRadius(T radius) { _radius = radius; }
+
+    std::string toString() const { return "[ " + _center.toString() + ", " + std::to_string(_radius) + " ]"; }
+
+private:
+    Offset<T> _center;
+    T _radius;
+};
+
 
 class Geometry {
 public:
