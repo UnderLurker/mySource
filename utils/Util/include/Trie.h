954d678a085ba4faed219ebcedea98f18fc27e77
UnderLurker <lnucxn@gmail.com>
2024-10-06 08:50:32 +0800

    新增前缀树

--- utils/Util/include/Trie.h
+++ utils/Util/include/Trie.h
@@ -5,13 +5,64 @@
 #ifndef _TRIE_H
 #define _TRIE_H
 
-#include "Tree.h"
+#include <map>
+#include <string>
 
 namespace myUtil {
 
-template<typename T, typename Compare = std::less<T>>
-class Trie : public Tree<T, Compare> {
+class Trie {
+public:
+    Trie() = default;
+    virtual ~Trie() {
+        for (auto& [key, value] : _table) {
+            if (value) delete value;
+        }
+    }
+    void insert(const std::string& word) {
+        if (word.empty()) return;
+        insert(word.c_str());
+    }
+
+    bool search(const std::string& word) {
+        if (word.empty()) return true;
+        bool flag = false;
+        return search(word.c_str(), word.size(), flag);
+    }
+
+    bool startsWith(const std::string& prefix) {
+        if (prefix.empty()) return true;
+        bool flag = false;
+        return startsWith(prefix.c_str(), prefix.size(), flag);
+    }
+
 private:
+    void insert(const char* word) {
+        if (!word || word[0] == '\0') {
+            _end = true;
+            return;
+        } else if (_table.find(word[0]) == _table.end()) {
+            _table[word[0]] = new Trie();
+        }
+        _table[word[0]]->insert(word + 1);
+    }
+
+    bool search(const char* word, size_t length, bool& flag) {
+        if (!word || length <= 0) return true;
+        else if (_table.find(word[0]) == _table.end()) return false;
+        flag = _table[word[0]]->_end;
+        return _table[word[0]]->search(word + 1, length - 1, flag) && flag;
+    }
+
+    bool startsWith(const char* word, size_t length, bool& flag) {
+        if (!word || length <= 0) return true;
+        else if (_table.find(word[0]) == _table.end()) return false;
+        flag = _table[word[0]]->_end;
+        return _table[word[0]]->startsWith(word + 1, length - 1, flag) && (flag || !_table[word[0]]->_table.empty());
+    }
+
+private:
+    std::map<char, Trie*> _table;
+    bool _end {false};
 };
 
 } // namespace myUtil
--- utils/Util/test/Util_test.cpp
+++ utils/Util/test/Util_test.cpp
@@ -1,29 +1,28 @@
 // #include "Util.h"
-#include <algorithm>
-#include <cstring>
 #include <functional>
+#include <gtest/gtest.h>
 #include <iostream>
-#include <locale>
 #include <map>
 #include <utility>
 
+#include "base64.h"
 #include "BTree.h"
 #include "heap.h"
 #include "RBTree.h"
 #include "SingleLinkList.h"
-#include "base64.h"
+#include "Trie.h"
 using namespace std;
 using namespace myUtil;
 
 void base64() {
     string str = Base64::encoding("123");
-    cout<<str<<endl;
+    cout << str << endl;
     string str1 = Base64::decoding(str);
-    cout<<str1<<endl;
+    cout << str1 << endl;
 }
 
 void rbtree() {
-    RBTree<int, int,less<>> p;
+    RBTree<int, int, less<>> p;
     // p.insert(1,1);
     // p.insert(2,1);
     // p.insert(3,1);
@@ -31,54 +30,53 @@ void rbtree() {
     // p.insert(5,1);
     // p.insert(6,1);
     // p.insert(7,1);
-    p.insert(pair<int, int>(1,1));
-    p.insert(pair<int, int>(2,1));
-    p.insert(pair<int, int>(3,1));
-    p.insert(pair<int, int>(4,1));
-    p.insert(pair<int, int>(5,1));
-    p.insert(pair<int, int>(6,1));
-    p.insert(pair<int, int>(7,1));
+    p.insert(pair<int, int>(1, 1));
+    p.insert(pair<int, int>(2, 1));
+    p.insert(pair<int, int>(3, 1));
+    p.insert(pair<int, int>(4, 1));
+    p.insert(pair<int, int>(5, 1));
+    p.insert(pair<int, int>(6, 1));
+    p.insert(pair<int, int>(7, 1));
     p.print();
 }
 
 void singleLinkList() {
     SingleLinkList<int> list;
-    for(int i=0;i<4;i++){
+    for (int i = 0; i < 4; i++) {
         list.push(i + 1);
     }
-    cout<<endl;
-    cout<<"length:"<<list.length()<<endl;
-    for(auto & it : list){
-        cout << it.value()<<" ";
+    cout << endl;
+    cout << "length:" << list.length() << endl;
+    for (auto& it : list) {
+        cout << it.value() << " ";
     }
-    cout<<endl;
+    cout << endl;
 
     SingleLinkList<int> a(list);
-    cout<<"length:"<<a.length()<<endl;
-    for(auto it = a.begin(); it != a.end(); it++){
-        cout << it->value()<<" ";
+    cout << "length:" << a.length() << endl;
+    for (auto it = a.begin(); it != a.end(); it++) {
+        cout << it->value() << " ";
     }
 
-    vector<int> b = {1,2,3,4,5};
+    vector<int> b = {1, 2, 3, 4, 5};
     SingleLinkList<int> c(&(*b.begin()), &(*b.end()));
-    cout<<endl;
-    cout<<"length:"<<c.length()<<endl;
-    for(auto it = c.begin(); it != c.end(); it++){
-        cout << it->value()<<" ";
+    cout << endl;
+    cout << "length:" << c.length() << endl;
+    for (auto it = c.begin(); it != c.end(); it++) {
+        cout << it->value() << " ";
     }
 
-    cout<<endl;
+    cout << endl;
     auto d = list;
-    cout<<"length:"<<d.length()<<endl;
-    for(auto it = d.begin(); it != d.end(); it++){
-        cout << it->value()<<" ";
+    cout << "length:" << d.length() << endl;
+    for (auto it = d.begin(); it != d.end(); it++) {
+        cout << it->value() << " ";
     }
 }
 
-void heapTest()
-{
-    vector<int> input { 5, 3, 10, 2, 1, 0, 8, 4 };
-    heap<int, less<>> a;// or heap<int, greater<>> a
+void heapTest() {
+    vector<int> input {5, 3, 10, 2, 1, 0, 8, 4};
+    heap<int, less<>> a; // or heap<int, greater<>> a
     a.print();
     for (int i = 10; i >= -5; i--) {
         a.push_back(i);
@@ -119,9 +117,25 @@ void heapTest()
     a.print("pop_front");
 }
 
-int main(){
+class UtilTest : public testing::Test {
+public:
+    void SetUp() override {}
 
-//    singleLinkList();
-//    heapTest();
-    return 0;
+    void TearDown() override {}
+};
+
+TEST_F(UtilTest, UtilTest001) {
+    std::string test1 = "apple";
+    std::string test2 = "apply";
+    std::string test3 = "app";
+    Trie dict;
+    dict.insert(test1);
+    dict.insert(test2);
+    EXPECT_TRUE(dict.search(test1));
+    EXPECT_TRUE(dict.startsWith(test3));
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
