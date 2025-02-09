5ec1aca705059c1f319af70d626661745e9c78c6
UnderLurker <lnucxn@gmail.com>
2025-02-09 16:44:23 +0800

    重构解析逻辑，使box解析逻辑独立

--- utils/Music/include/m4a.h
+++ utils/Music/include/m4a.h
@@ -14,12 +14,15 @@
 
 namespace myUtil {
 
+#define FULL_BOX_LEN   4
 #define BOX_HEADER_LEN 8
+#define USER_TYPE_LEN  16
 #define CONVERT(a)     (((int32_t)(#a[0]) << 24) + ((int32_t)(#a[1]) << 16) + ((int32_t)(#a[2]) << 8) + (int32_t)(#a[3]))
 
 enum BoxType : uint32_t {
     DINF = CONVERT(dinf),
     DREF = CONVERT(dref),
+    FREE = CONVERT(free),
     FTYP = CONVERT(ftyp),
     HDLR = CONVERT(hdlr),
     MDAT = CONVERT(mdat),
@@ -29,6 +32,8 @@ enum BoxType : uint32_t {
     MOOF = CONVERT(moof),
     MOOV = CONVERT(moov),
     MVHD = CONVERT(mvhd),
+    PDIN = CONVERT(pdin),
+    SKIP = CONVERT(skip),
     STBL = CONVERT(stbl),
     STCO = CONVERT(stco),
     STSC = CONVERT(stsc),
@@ -37,68 +42,104 @@ enum BoxType : uint32_t {
     TKHD = CONVERT(tkhd),
     TRAK = CONVERT(trak),
     TREF = CONVERT(tref),
+    UUID = CONVERT(uuid),
     VMHD = CONVERT(vmhd),
 };
 
 struct BoxHeader {
     uint64_t size {0};
     BoxType type;
+    uint8_t version;
+    uint32_t flags;
     std::string TypeToString() const;
     void SetValue(const uint8_t* data, size_t length);
     void SetBigSize(const uint8_t* data, size_t length);
     uint64_t BodySize() const { return size > UINT32_MAX ? size - BOX_HEADER_LEN * 2 : size - BOX_HEADER_LEN; }
 };
 
+// Container
 class Box {
 public:
-    Box()          = default;
-    virtual ~Box() = default;
+    using BoxPair = std::pair<BoxType, std::shared_ptr<Box>>;
+    Box()         = default;
+    virtual ~Box() { delete[] userType; }
 
-    virtual MusicStatus GetData(std::fstream& file) {
-        try {
-            auto data = new uint8_t[BOX_HEADER_LEN];
-            file.read((char*)data, BOX_HEADER_LEN);
-            _header.SetValue(data, BOX_HEADER_LEN);
-            if (_header.size == 1) {
-                file.read((char*)data, BOX_HEADER_LEN);
-                _header.SetBigSize(data, BOX_HEADER_LEN);
-            }
-            delete[] data;
-        } catch (...) { return ERROR_UNKNOWN; }
-        return SUCCESS;
-    }
+    virtual MusicStatus ProcessHeader(std::fstream& file);
+    // Analyze Box with SubBox
+    virtual MusicStatus ProcessData(std::fstream& file);
+    void ProcessFullBox(std::fstream& file);
+    void ProcessFullBox(const uint8_t* body, size_t length);
 
 public:
     BoxHeader _header {};
+    std::list<BoxPair> boxes;
+    uint8_t* userType {};
 };
-using BoxPair = std::pair<BoxType, std::shared_ptr<Box>>;
 
-struct FtypBox : public Box {
+// Not container
+class LeafBox : public virtual Box {
+public:
+    // Analyze Box
+    MusicStatus ProcessData(std::fstream& file) override;
+
+protected:
+    /**
+     * Assignment Data
+     * @param body file data(exclude BoxHeader)
+     * @param length
+     * @return
+ */
+    virtual MusicStatus OnProcessData(const uint8_t* body, size_t length) { return SUCCESS; }
+};
+
+// File Type Box
+struct FtypBox : public LeafBox {
     uint32_t majorBrand;
     uint32_t minorVersion;
     std::vector<uint32_t> compatibleBrands;
-    MusicStatus GetData(std::fstream& file) override;
+    MusicStatus OnProcessData(const uint8_t* body, size_t length) override;
 };
 
-struct MvhdBox : public Box {
-    uint8_t version;
-    uint32_t flags: 24;
-    uint32_t createTime;
-    uint32_t modificationTime;
+// Movie Header Box
+struct MvhdBox : public LeafBox {
+    uint64_t createTime;
+    uint64_t modificationTime;
     uint32_t timeScale;
-    uint32_t duration;
-    uint32_t rate;
-    uint16_t volume;
-    uint8_t reserved[10];
-    uint8_t matrix[36];
-    uint8_t preDefined[24];
+    uint64_t duration;
+    uint32_t rate {0x00010000};
+    int16_t volume {0x0100};
+    uint8_t reserved[10] {0};
+    int32_t matrix[9] {0x00010000, 0, 0, 0, 0x00010000, 0, 0, 0, 0x40000000};
+    int32_t preDefined[6] {0};
     uint32_t nextTrackId;
-    MusicStatus GetData(std::fstream& file) override;
+    MusicStatus OnProcessData(const uint8_t* body, size_t length) override;
 };
 
-struct MoovBox : public Box {
-    std::list<BoxPair> boxes;
-    MusicStatus GetData(std::fstream& file) override;
+struct MoovBox : public Box {};
+
+// Media Data Box
+struct MdatBox : public LeafBox {
+    std::unique_ptr<uint8_t[]> data;
+    size_t len {0};
+    MusicStatus OnProcessData(const uint8_t* body, size_t length) override;
+};
+
+// Free Space Box (Can ignore)
+struct FreeBox : public MdatBox {};
+// Free Space Box (Can ignore)
+struct SkipBox : public MdatBox {};
+
+// Progressive Download Information Box
+struct PdinBox : public LeafBox {
+    struct PdinInfo {
+        // a download rate expressed in bytes/second
+        uint32_t rate;
+        // InitialDelay is the suggested delay to use when playing the file, such that if download continues at the
+        // given rate, all data within the file will arrive in time for its use and playback should not need to stall.
+        uint32_t initialDelay;
+    };
+    std::list<PdinInfo> infos;
+    MusicStatus OnProcessData(const uint8_t* body, size_t length) override;
 };
 
 class M4a : public MusicBase {
@@ -110,7 +151,7 @@ public:
     MusicStatus analysis() override;
 
 private:
-    std::list<BoxPair> _boxes;
+    std::list<Box::BoxPair> _boxes;
 };
 
 } // namespace myUtil
--- utils/Music/src/m4a.cpp
+++ utils/Music/src/m4a.cpp
@@ -16,7 +16,11 @@ uint64_t GetValue(const uint8_t* data, size_t begin, size_t end) {
 std::map<BoxType, std::function<std::shared_ptr<Box>()>> BoxMap = {
     {myUtil::BoxType::FTYP, []() { return std::make_shared<FtypBox>(); }},
     {myUtil::BoxType::MOOV, []() { return std::make_shared<MoovBox>(); }},
-    {myUtil::BoxType::MVHD, []() { return std::make_shared<MvhdBox>(); }}
+    {myUtil::BoxType::MVHD, []() { return std::make_shared<MvhdBox>(); }},
+    {myUtil::BoxType::MDAT, []() { return std::make_shared<MdatBox>(); }},
+    {myUtil::BoxType::FREE, []() { return std::make_shared<FreeBox>(); }},
+    {myUtil::BoxType::SKIP, []() { return std::make_shared<SkipBox>(); }},
+    {myUtil::BoxType::PDIN, []() { return std::make_shared<PdinBox>(); }},
 };
 } // namespace
 
@@ -42,46 +46,25 @@ void BoxHeader::SetBigSize(const uint8_t* data, size_t length) {
     size = GetValue(data, 0, 8);
 }
 
-MusicStatus FtypBox::GetData(std::fstream& file) {
-    auto remainLen = _header.BodySize();
-    if (remainLen < 8) return ERROR_FILE_FORMAT;
-    auto body = new uint8_t[remainLen];
-    file.read((char*)body, remainLen);
-    majorBrand    = GetValue(body, 0, 4);
-    minorVersion  = GetValue(body, 4, 8);
-    remainLen    -= 8;
-    auto start    = 8;
-    while (remainLen > 0) {
-        compatibleBrands.push_back(GetValue(body, start, start + sizeof(uint32_t)));
-        remainLen -= 4;
-        start     += 4;
-    }
-    delete[] body;
+MusicStatus Box::ProcessHeader(std::fstream& file) {
+    try {
+        auto data = new uint8_t[BOX_HEADER_LEN];
+        file.read((char*)data, BOX_HEADER_LEN);
+        _header.SetValue(data, BOX_HEADER_LEN);
+        if (_header.size == 1) {
+            file.read((char*)data, BOX_HEADER_LEN);
+            _header.SetBigSize(data, BOX_HEADER_LEN);
+        }
+        if (_header.type == myUtil::BoxType::UUID) {
+            userType = new uint8_t[USER_TYPE_LEN];
+            file.read((char*)userType, 16);
+        }
+        delete[] data;
+    } catch (...) { return ERROR_UNKNOWN; }
     return SUCCESS;
 }
 
-MusicStatus MvhdBox::GetData(std::fstream& file) {
-    auto remainLen = _header.BodySize();
-    if (remainLen < 8) return ERROR_FILE_FORMAT;
-    auto body = new uint8_t[remainLen];
-    file.read((char*)body, remainLen);
-    version          = GetValue(body, 0, 1);
-    flags            = GetValue(body, 1, 4);
-    createTime       = GetValue(body, 4, 8);
-    modificationTime = GetValue(body, 8, 12);
-    timeScale        = GetValue(body, 12, 16);
-    duration         = GetValue(body, 16, 20);
-    rate             = GetValue(body, 20, 24);
-    volume           = GetValue(body, 24, 26);
-    nextTrackId      = GetValue(body, 96, 100);
-    memcpy(reserved, body + 26, 10);
-    memcpy(matrix, body + 36, 36);
-    memcpy(preDefined, body + 72, 24);
-    delete[] body;
-    return SUCCESS;
-}
-
-MusicStatus MoovBox::GetData(std::fstream& file) {
+MusicStatus Box::ProcessData(std::fstream& file) {
     int64_t remainLen = _header.BodySize();
     if (remainLen < 8) return ERROR_FILE_FORMAT;
     while (remainLen > 0) {
@@ -92,9 +75,9 @@ MusicStatus MoovBox::GetData(std::fstream& file) {
         ss >> pos;
 #endif
         Box box;
-        box.GetData(file);
+        box.ProcessHeader(file);
 #ifdef UTIL_DEBUG
-        LOGI(" SIZE:0x%x, \ttype:%s, pos:0x%x", box._header.size, box._header.TypeToString().c_str(), pos);
+        LOGI(" Box SIZE:0x%x, \ttype:%s, pos:0x%x", box._header.size, box._header.TypeToString().c_str(), pos);
 #endif
         auto bodySize = box._header.BodySize();
         auto iter     = BoxMap.find(box._header.type);
@@ -103,10 +86,100 @@ MusicStatus MoovBox::GetData(std::fstream& file) {
         } else {
             auto subBox     = iter->second();
             subBox->_header = box._header;
-            subBox->GetData(file);
+            subBox->ProcessData(file);
             boxes.emplace_back(subBox->_header.type, subBox);
         }
         remainLen -= bodySize;
+    }
+    return SUCCESS;
+}
+
+void Box::ProcessFullBox(std::fstream& file) {
+    auto data = new int8_t[4];
+    file.read((char*)data, 4);
+    ProcessFullBox(reinterpret_cast<const uint8_t*>(data), 4);
+    delete[] data;
+}
+void Box::ProcessFullBox(const uint8_t* body, size_t length) {
+    _header.version = body[0];
+    _header.flags   = GetValue(body, 1, 4);
+}
+
+MusicStatus LeafBox::ProcessData(std::fstream& file) {
+    auto remainLen = _header.BodySize();
+    if (remainLen < 8) return ERROR_FILE_FORMAT;
+    auto body = new uint8_t[remainLen];
+    file.read((char*)body, remainLen);
+    auto ret = OnProcessData(body, remainLen);
+    delete[] body;
+    return ret;
+}
+
+MusicStatus FtypBox::OnProcessData(const uint8_t* body, size_t length) {
+    majorBrand    = GetValue(body, 0, 4);
+    minorVersion  = GetValue(body, 4, 8);
+    length       -= 8;
+    auto start    = 8;
+    while (length > 0) {
+        compatibleBrands.push_back(GetValue(body, start, start + sizeof(uint32_t)));
+        length -= sizeof(uint32_t);
+        start  += sizeof(uint32_t);
+    }
+    return SUCCESS;
+}
+
+MusicStatus MvhdBox::OnProcessData(const uint8_t* body, size_t length) {
+    ProcessFullBox(body, length);
+    if ((_header.version == 1 && length != 112) || (_header.version == 0 && length != 100)) return ERROR_FILE_FORMAT;
+    auto pos = 0;
+    if (_header.version == 1) {
+        createTime       = GetValue(body + FULL_BOX_LEN, 0, 8);
+        modificationTime = GetValue(body + FULL_BOX_LEN + 8, 0, 8);
+        timeScale        = GetValue(body + FULL_BOX_LEN + 16, 0, 4);
+        duration         = GetValue(body + FULL_BOX_LEN + 20, 0, 8);
+        pos              = FULL_BOX_LEN + 28;
+    } else { // version == 0
+        createTime       = GetValue(body + FULL_BOX_LEN, 0, 4);
+        modificationTime = GetValue(body + FULL_BOX_LEN + 4, 0, 4);
+        timeScale        = GetValue(body + FULL_BOX_LEN + 8, 0, 4);
+        duration         = GetValue(body + FULL_BOX_LEN + 12, 0, 4);
+        pos              = FULL_BOX_LEN + 16;
+    }
+    rate        = GetValue(body + pos, 0, 4);
+    volume      = GetValue(body + pos + 4, 0, 2);
+    nextTrackId = GetValue(body + pos + 76, 0, 4);
+    memcpy(reserved, body + pos + 6, 10);
+    memcpy(matrix, body + pos + 16, 36);
+    for (size_t i = 0; i < sizeof(matrix) / sizeof(int32_t); i++) {
+        auto tmp  = body + pos + 16 + 4 * i;
+        matrix[i] = GetValue(tmp, 0, 4);
+    }
+    for (size_t i = 0; i < sizeof(preDefined) / sizeof(int32_t); i++) {
+        auto tmp      = body + pos + 52 + 4 * i;
+        preDefined[i] = GetValue(tmp, 0, 4);
+    }
+    return SUCCESS;
+}
+
+MusicStatus MdatBox::OnProcessData(const uint8_t* body, size_t length) {
+    data = std::make_unique<uint8_t[]>(length);
+    memcpy(data.get(), body, length);
+    len = length;
+    return SUCCESS;
+}
+
+MusicStatus PdinBox::OnProcessData(const uint8_t* body, size_t length) {
+    ProcessFullBox(body, length);
+    size_t size = sizeof(PdinInfo);
+    if (length - FULL_BOX_LEN % size != 0) return ERROR_FILE_FORMAT;
+    size_t l = 0 + FULL_BOX_LEN, r = 4 + FULL_BOX_LEN;
+    while (length) {
+        uint32_t rate         = GetValue(body, l, r);
+        uint32_t initialDelay = GetValue(body, l + 4, r + 4);
+        infos.push_back({rate, initialDelay});
+        length -= size;
+        l      += size;
+        r      += size;
     }
     return SUCCESS;
 }
@@ -126,15 +199,15 @@ MusicStatus M4a::analysis() {
             ss >> pos;
 #endif
             Box box;
-            box.GetData(file);
+            box.ProcessHeader(file);
 #ifdef UTIL_DEBUG
-            LOGI(" SIZE:0x%x, \ttype:%s, pos:0x%x", box._header.size, box._header.TypeToString().c_str(), pos);
+            LOGI(" File SIZE:0x%x, \ttype:%s, pos:0x%x", box._header.size, box._header.TypeToString().c_str(), pos);
 #endif
             auto iter = BoxMap.find(box._header.type);
             if (iter != BoxMap.end()) {
                 auto subBox     = iter->second();
                 subBox->_header = box._header;
-                subBox->GetData(file);
+                subBox->ProcessData(file);
                 _boxes.emplace_back(subBox->_header.type, subBox);
             } else {
                 file.seekg(box._header.BodySize(), std::ios::cur);
